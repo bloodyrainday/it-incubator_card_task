0d97029c9b6d41f309015f5335991e4dd8f3575d
bloodyrainday <bloodyrainday@gmail.com>
2024-12-05 15:29:54 +0300

    init: start a project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="App">card</div>;
+}
+
+export default App;
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *:: before,
+    *:: after {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    body {
+    font-family: "Inter", sans-serif}
+`;
